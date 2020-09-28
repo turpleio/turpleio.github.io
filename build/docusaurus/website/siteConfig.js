f49d976e2495449961c1b85449bc68613acc210f
mikesung <mksung@gitple.com>
2020-09-28 14:18:15 +0900

    add docs as default

--- build/docusaurus/website/siteConfig.js
+++ build/docusaurus/website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Turple Docs', // Title for your website.
   tagline: '',
   url: 'https://turpleio.github.io', // Your website URL
-  baseUrl: '/guide/', // Base URL for your project */
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
